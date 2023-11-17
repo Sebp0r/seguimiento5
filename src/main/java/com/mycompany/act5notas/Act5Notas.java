8873e2dfe1ce2dd24630a8b6ffef955dfd3815ab
Sebp0r <juposadar@unal.edu.co>
2023-11-17 11:39:33 -0500

    Ejercicio1
    Seg5Ej1 Notas

---
+++ src/main/java/com/mycompany/act5notas/Act5Notas.java
@@ -0,0 +1,13 @@
+package com.mycompany.act5notas;
+/**
+ *
+ * @author Sebastian Posada
+ */
+public class Act5Notas {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
